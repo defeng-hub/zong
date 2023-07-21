7ecf1d91daf290f12a940b0f030941403c0cf32e
wangdefeng_i <wangdefeng_i@didiglobal.com>
2023-07-21 17:18:58 +0800

    强制类型转换

--- mage_go/13-2-switchType/main.go
+++ mage_go/13-2-switchType/main.go
@@ -4,11 +4,23 @@ import "fmt"
 
 func main() {
 	var num interface{} = "aa"
+
+	// switch type
+	//switch value := num.(type){}
 	switch value := num.(type) {
 	case int:
-		fmt.Println(value)
+		fmt.Printf("int:%d", value)
 	case string:
 		fmt.Printf("string: %s", value)
+	case float64:
+		fmt.Printf("float: %.2f", value)
 	}
 
+	//	switch type 和 强制类型转换
+	// num.(int)
+	switch num.(type) {
+	case int:
+		value := num.(int)
+		fmt.Printf("强制类型转换，int:%d", value)
+	}
 }
