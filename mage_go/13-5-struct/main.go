094f1ec98d02b90ba16e89d074feefb2578bc5d2
wangdefeng_i <wangdefeng_i@didiglobal.com>
2023-07-22 12:51:03 +0800

    push

--- mage_go/13-5-struct/main.go
+++ mage_go/13-5-struct/main.go
@@ -18,9 +18,48 @@ type usermap map[int]string
 func (um usermap) getaa() {
 
 }
+
+type Stu struct {
+	name   string
+	yuwen  float64
+	shuxue float64
+	yingyu float64
+	avg    float64
+}
+type cclass struct {
+	students []*Stu
+	yuwen    float64
+	shuxue   float64
+	yingyu   float64
+}
+
 func main() {
 	// 结构体每个成员 是有默认值的
 	user := User{time: time.Now()}
 	fmt.Printf("[%s]-[%d]-[%f]-[%d]", user.name, user.age, user.height, user.time)
+
+	s1 := Stu{
+		name:   "s1",
+		yuwen:  36,
+		shuxue: 47,
+		yingyu: 58,
+	}
+	s2 := Stu{
+		name:   "s2",
+		yuwen:  36,
+		shuxue: 47,
+		yingyu: 58,
+	}
+	s3 := Stu{
+		name:   "s3",
+		yuwen:  36,
+		shuxue: 47,
+		yingyu: 58,
+	}
+	cla := cclass{
+		students: []*Stu{&s1, &s2, &s3},
+	}
+
+	fmt.Printf("%v", cla)
 
 }
