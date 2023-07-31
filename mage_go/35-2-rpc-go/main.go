c39b4aacc404fc77049908ee487f4ec0aedeb64f
defeng <15555150781@163.com>
2023-07-31 19:58:54 +0800

    p

--- mage_go/35-2-rpc-go/main.go
+++ mage_go/35-2-rpc-go/main.go
@@ -1,1 +1,28 @@
-package _5_2_rpc_go
+package main
+
+import (
+	"net"
+	"net/rpc"
+)
+
+type HelloService struct {
+}
+
+func (h *HelloService) Hello(req string, res *string) error {
+	*res = "hello" + req
+	return nil
+}
+
+func main() {
+	rpc.RegisterName("HelloService", new(HelloService))
+
+	listen, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		return
+	}
+	for {
+		conn, _ := listen.Accept()
+
+		go rpc.ServeConn(conn)
+	}
+}
--- mage_go/35-4/main.go
+++ mage_go/35-4/main.go
@@ -1,1 +1,19 @@
-package _5_4
+package main
+
+import (
+	"sync"
+)
+
+type singleton struct {
+}
+
+var instance *singleton
+var once sync.Once
+
+func main() {
+	instance = new(singleton)
+	println(instance)
+	instance = new(singleton)
+	println(instance)
+	return
+}
